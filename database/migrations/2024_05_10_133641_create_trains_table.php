9a33de76d52bc469646a55e97de4df06de6883dd
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-10 16:05:50 +0200

    add trains table with basic minimum columns structure through migrate

--- database/migrations/2024_05_10_133641_create_trains_table.php
+++ database/migrations/2024_05_10_133641_create_trains_table.php
@@ -4,6 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/* 
+Ogni treno dovrà avere almeno:
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+*/
+
 return new class extends Migration
 {
     /**
@@ -13,6 +26,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_Station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 20);
+            $table->integer('coach_count')->unsigned()->nullable();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
