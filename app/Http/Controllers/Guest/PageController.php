12c986033244b0a336f9ff5e8f9ae037faca8d2c
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-10 17:03:59 +0200

    correctly connected and dd'ed trains table to template

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $trains = Train::all();
+
+        return view('guests.welcome', compact('trains'));
     }
 }
