e009e93767987f390ede6553fe3e1e35aa874549
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-10 15:43:33 +0200

    add basic header and footer, styles and assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel Migration Seeder</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+  <!-- Styles -->
+  @vite('resources/js/app.js')
+
+  {{-- bootstrap, fontawesome --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  {{-- favicon --}}
+  <link rel="icon" type="image/png" href="{{ asset('img/logomark.min.svg') }}" />
+  {{-- font --}}
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
 </head>
 <body>
   <header>
     @include('partials.header')
   </header>
   <main class="py-4 mb-4">
-    yield('content')
+    @yield('content')
   </main>
   <footer>
     @include('partials.footer')
